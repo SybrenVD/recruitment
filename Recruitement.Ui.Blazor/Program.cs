695315bce6836e57032a7d05f36e6b0edefd680b
reinh <Reinhardvandenbulcke@hotmail.com>
2026-03-27 15:36:56 +0100

    SDK + HTTP factory added

--- Recruitement.Ui.Blazor/Program.cs
+++ Recruitement.Ui.Blazor/Program.cs
@@ -1,11 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Recruitement.Ui.Blazor;
+using Recruitement.Ui.Blazor.ApiClients;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// Register typed API client for Candidates
+builder.Services.AddHttpClient<ICandidateApiClient, CandidateApiClient>(client =>
+{
+    client.BaseAddress = new Uri("https://localhost:5001/api/candidates");
+});
 
 await builder.Build().RunAsync();
